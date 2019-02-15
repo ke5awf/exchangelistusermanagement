71ebdb3e8964fe8d8395d08e7563a0f68498c3ec
ke5awf <ke5awf@gmail.com>
2019-02-15 16:29:55 -0600

    Upload of Files

--- README.md
+++ README.md
@@ -4,11 +4,13 @@ Script to sync lists of users from SQL and add AD users to exchange distribution
 
 This is a script I made in an effort to eliminate an old mailman server. 
 
-The Idea is to run this script at some regular interval, in my case nightly to take a list of users based on an SQL query, add those users to an exchange distrobution group. 
+The Idea is to run this script at some regular interval, in my case nightly to take a list of users based on an SQL query, add those users to an exchange distribution group. 
 
 
 This script assumes makes the assumption you have the rights to make these modifications to the exchange server via the powershell. 
 
 
 If you have any suggestions to modifications feel free to post them up. 
+
+Included is also an example Task for the task scheduler, that needs to be modified with User_ID, User_ID_string, and location of file. 
 Keith
---
+++ maillistsync.ps1
@@ -0,0 +1,51 @@
+#10/10/2018
+#Script for Syncing Emails from SQL to Distro list in Exchange
+#Only Exchange Admin's should attempt to run this script
+
+$SYNCNAME = "All Mailing Lists"
+#Name of Sync
+
+#The following sets up the connection to Exchange, it will ask you for Exchange Admin Credentials to start the remote session
+$Username = "DOMAIN\USER"
+$Password = 'PASSWORD' | ConvertTo-SecureString -AsPlainText -Force
+Import-PSSession $Session -DisableNameChecking
+$UserCredential = New-Object System.Management.Automation.PSCredential -ArgumentList $Username,$Password
+#Or you can have it prompt you for the credentials by using the following line instead. 
+#The following sets up the connection to Exchange, it will ask you for Exchange Admin Credentials to start the remote session
+#$UserCredential = Get-Credential
+
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://EXCHANGE_FQDN/PowerShell/ -Authentication Kerberos -Credential $UserCredential
+Import-PSSession $Session -DisableNameChecking
+
+#Declare group list that the Users will be put into. 
+#There is an assumption that this group exists already in exchange, if it does not you must create it first!!
+$group1="DISTRO1"
+
+
+
+#Get List of emails from SQL that need to be in this sync, plus any extras that need manually added.
+#DISTRO1
+$emaillist1 = (Invoke-SQLcmd -server 'SQL_SERVER' -database 'DATABASE' -Query "select email from database.dbo.maillist where listname = 'DISTRO1' order by lastname, firstname")
+
+
+
+#Remove users from list that are currently in the list. This is being done with the update-distibutiongroupmember command.
+Update-DistributionGroupMember -Identity $group1 -Members user@domain.com -Confirm:$false
+
+
+
+
+#Get results of SQL and aux list and add email to specified group. 
+$emaillist1 | ForEach {Add-DistributionGroupMember -Identity $group1 -Member $_.email}
+Add-DistributionGroupMember -Identity $group1 -Member "specialadd@domain.com"
+
+
+#required to exit SQLServer
+cd c:
+
+
+#Save results to \\FILESERVER\DESTINATION and email a copy to the admin
+Get-DistributionGroupmember -identity $group1 | select DisplayName,PrimarySMTPAddress | Export-CSV "C:\MailLists\Export\$group1 $(get-date -f yyyy-MM-dd).log"
+Send-MailMessage -SMTPServer SMTP_SERVER -To "emailadmin@domain.com" -From "EMAILSYNC@domain.com" -Subject "Distro List SYNC Complete $SYNCNAME" -Body "Sync results can be found in \\FILESERVER\DESTINATION" -Attachment "C:\MailLists\Export\$group1 $(get-date -f yyyy-MM-dd).log"
+
+exit
